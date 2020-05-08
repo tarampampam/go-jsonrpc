abae1afd082ca3881f2011be32ab731334733425
Paramtamtam <slashkot@gmail.com>
2020-05-08 11:13:22 +0500

    Some code comments updated

--- errors/errors.go
+++ errors/errors.go
@@ -8,11 +8,12 @@ import (
 type Code int
 
 const (
-	Parse          Code = -32700 // Parse error
-	InvalidRequest Code = -32600 // Invalid Request
-	MethodNotFound Code = -32601 // Method not found
-	InvalidParams  Code = -32602 // Invalid params
-	Internal       Code = -32603 // Internal error
+	// RPC error codes
+	Parse          Code = -32700
+	InvalidRequest Code = -32600
+	MethodNotFound Code = -32601
+	InvalidParams  Code = -32602
+	Internal       Code = -32603
 )
 
 // Error is a wrapper for a JSON interface value. Docs: <https://www.jsonrpc.org/specification#error_object>
